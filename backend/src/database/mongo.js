35c2fa14ff49e3abbfd6575fd74a1c1ac7ceab2d
Rafael Calmon Gonzaga Santos <rafacalmon27@gmail.com>
2026-05-04 17:21:25 -0300

    Primeiro commit (Conexão mongoDB)

---
+++ backend/src/database/mongo.js
@@ -0,0 +1,21 @@
+import {MongoClient} from 'mongodb'
+
+export const Mongo = {
+    async connect({mongoConnectionString, mongoDbName}) {
+        try {
+            const client = new MongoClient(mongoConnectionString)
+
+            await client.connect()
+            const db = client.db(mongoDbName)
+
+            this.client = client
+            this.db = db
+
+            return 'Connected to Mongo successfully'
+            
+        } catch (error) {
+            return { text: 'Error during mongo connection', error }
+        }
+
+    }
+}
