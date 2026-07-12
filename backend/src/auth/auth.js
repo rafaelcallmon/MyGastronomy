99318048c3881667e0f78883c47fd1692a83c49a
Rafael Calmon Gonzaga Santos <rafacalmon27@gmail.com>
2026-07-12 12:14:35 -0300

    CRUD users

--- backend/src/auth/auth.js
+++ backend/src/auth/auth.js
@@ -115,7 +115,7 @@ authRouter.post('/login', async (req, res) => {
                 success: false,
                 statusCode: 400,
                 body: {
-                    text: 'User not found'
+                    text: 'Credentials are not correct'
                 }
             })
         }
