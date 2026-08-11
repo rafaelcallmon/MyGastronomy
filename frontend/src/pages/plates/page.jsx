b1602013c1c7c613a2003a24b4a79c615d6b2603
Rafael Calmon Gonzaga Santos <rafacalmon27@gmail.com>
2026-08-11 16:30:08 -0300

    UseContext para o Carrinho

--- frontend/src/pages/plates/page.jsx
+++ frontend/src/pages/plates/page.jsx
@@ -1,16 +1,30 @@
 import platesServices from "../../services/plates.jsx"
 import styles from "./page.module.css"
 import Loading from "../loading/page.jsx"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { Navigate } from "react-router-dom"
 import PlateCard from "../../components/plateCard/plateCard.jsx"
+import PlatePopup from "../../components/platePopup/platePopup.jsx"
+import { useCartContext } from "../../contexts/useCartContext.jsx"
 
 export default function Plates() {
     const { getAvailablePlates, platesList, platesLoading, refetchPlates } = platesServices()
+    const [plateSelected, setPlateSelected] = useState(null)
+    const { addToCart } = useCartContext()
 
     const authData = JSON.parse(localStorage.getItem('auth'))
 
-    if (!authData) {
-        return <Navigate to="/auth" replace />;
+    const handleSelectedPlate = (plate) => {
+        setPlateSelected(plate)
+    }
+
+    const handleClosePopup = () => {
+        setPlateSelected(null)
+    }
+
+    const handleAddToCart = (itemToAdd) => {
+        addToCart(itemToAdd);
+        handleClosePopup()
     }
 
     useEffect(() => {
@@ -19,20 +33,30 @@ export default function Plates() {
         }
     }, [refetchPlates]);
 
+    if (!authData) {
+        return <Navigate to="/auth" replace />;
+    }
+
     if (!platesList) {
         return ( <Loading></Loading> )
     }
 
-    console.log(platesList);
     
-
     return (
         <>
             <div>
                 {platesList.map((plate) => (
-                    <PlateCard plateData={plate} key={plate._id}></PlateCard>
+                    <div key={plate._id} className={styles.cardContainer} onClick={() => { handleSelectedPlate(plate) }}>
+                        <PlateCard plateData={plate}></PlateCard>
+                    </div>
                 ))}
             </div>
+
+            {plateSelected && (
+                <>
+                    <PlatePopup plateData={plateSelected} onClose={handleClosePopup} onAddToCart={handleAddToCart}/>  
+                </>
+            )}
         </>
     )
 }
